08e2951734a47ff669e1655f20bc8dce03c96f3d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-22 18:29:48 +0200

    create model using Schema constructor

--- server/models/post.js
+++ server/models/post.js
@@ -1,22 +1,28 @@
 const mongoose = require("mongoose");
 
-const postSchema = mongoose.Schema({
-	title: {
-		type: String,
-		required: true
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true
+		},
+		imageUrl: {
+			type: String,
+			required: true
+		},
+		content: {
+			type: String,
+			required: true
+		},
+		creator: {
+			type: Object
+		}
 	},
-	imageUrl: {
-		type: String,
-		required: true
-	},
-	content: {
-		type: String,
-		required: true
-	},
-	creator: {
-		type: Object,
-		required: true
+	{
+		timestamps: true
 	}
-});
+);
 
 module.exports = mongoose.model("Post", postSchema);
