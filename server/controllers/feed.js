a1385547343d67cd7ea5f394b7eebc77cf79925d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-23 22:57:39 +0200

    store posts on database

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator/check");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
@@ -25,18 +26,24 @@ exports.createPost = (req, res, next) => {
 			errors: errors.array()
 		});
 	}
-	const title = req.body.title;
-	const content = req.body.content;
-	res.status(201).json({
-		message: "Post created successfully",
-		post: {
-			_id: new Date().toISOString(),
-			title,
-			content,
-			creator: {
-				name: "Sahil"
-			},
-			createdAt: new Date()
+	const post = new Post({
+		title: req.body.title,
+		content: req.body.content,
+		imageUrl: "images/duck.jpg",
+		creator: {
+			name: "Sahil"
 		}
 	});
+	post
+		.save()
+		.then(result => {
+			console.log(result);
+			res.status(201).json({
+				message: "Post created successfully!",
+				post: result
+			});
+		})
+		.catch(err => {
+			console.log(err);
+		});
 };
