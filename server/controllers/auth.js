9f7cf268295e5f7f47118f60ddd809aaaff37793
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-25 15:47:48 +0200

    add login logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -27,6 +28,48 @@ exports.signup = (req, res, next) => {
 		})
 		.then(user => {
 			res.status(201).json({ message: "New user created", userId: user._id });
+		})
+		.catch(err => {
+			if (err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+exports.login = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	let loadedUser;
+	User.findOne({
+		email
+	})
+		.then(user => {
+			if (!user) {
+				const error = new Error("email or password is incorrect");
+				error.statusCode = 401;
+				throw error;
+			}
+			loadedUser = user;
+			return bcrypt.compare(password, user.password);
+		})
+		.then(isEqual => {
+			if (!isEqual) {
+				const error = new Error("email or password is incorrect");
+				error.statusCode = 401;
+				throw error;
+			}
+			const token = jwt.sign(
+				{
+					email: loadedUser.email,
+					userId: loadedUser._id.toString()
+				},
+				"ERG34wthbty%t54rhaw34556H%R^ewsrFGarfe#F$$#Ferg34r",
+				{
+					expiresIn: "1h"
+				}
+			);
+			res.status(200).json({ token, userId: loadedUser._id.toString() });
 		})
 		.catch(err => {
 			if (err.statusCode) {
